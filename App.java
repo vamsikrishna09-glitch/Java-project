804197b3f099e94cef50d3d55ba24ab67c90066b
vamsikrishna09-glitch <mvkkrishna09@gmail.com>
2026-05-12 11:53:56 +0530

    Create App.java with main and add methods

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, CI/CD World");
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum: " + add(a, b));
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
